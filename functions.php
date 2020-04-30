4a1781df475843b408eb9811d1b21fb0fc5fd2b6
misterInterlocked <kilian.schrenk@gmail.com>
2020-04-30 14:48:54 +0200

    uncompressed css

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+
+// custom styles
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), filemtime( get_stylesheet_directory() . '/style.css' ) );
 }
@@ -6,11 +8,3 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
 
-/* add jQuery to head 
-function hook_javascript() {
-    ?>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://raumausstatter-schrenk/wp-content/themes/divi-child/js/jquery-3.4.1.min.js"></script>
-    <?php
-}
-add_action('wp_head', 'hook_javascript'); */
