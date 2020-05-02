147297f2f519fa4277e0211559e696a8ebf434ff
misterInterlocked <kilian.schrenk@gmail.com>
2020-05-02 12:10:21 +0200

    enqueue child styles

--- functions.php
+++ functions.php
@@ -1,10 +1,20 @@
 <?php
 
 // custom styles
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), filemtime( get_stylesheet_directory() . '/style.css' ) );
+function add_theme_scripts() {
+    
+    // load parent styles
+    $parent_style = 'parent-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . 
+        '/style.css');
+        
+    // load child-theme styles
+    wp_enqueue_style('child-theme', 
+        get_stylesheet_directory_uri() . '/css/child-theme.css', 
+        array($parent_style),
+        filemtime(get_stylesheet_directory() . '/css/child-theme.css') );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
 
 
